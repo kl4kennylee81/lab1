923abe94e9f4abbf38425bfbf2cb364597b76cef
Kenneth Lee <kl488@en-cs-uglab03.coecis.cornell.edu>
2017-02-15 19:39:41 -0500

    hi

--- encode.c
+++ encode.c
@@ -71,6 +71,7 @@ static int encode(struct packet *packets, int cnt, uint64_t state, const int idl
 	/* assume no idle characters in the begining */
 	/* for each packets */
 	for (i = 0 ; i < cnt ; i ++ ) {
+		printf("Packet %d\n",i);
 		/* data is pointing to the first byte of the packet */
 		data = packets[i].eth_frame;
 		/* len is the length of the packet */
